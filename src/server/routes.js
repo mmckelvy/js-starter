e8095a47c818a5cb6ac60048f8563372263994ea
Mark McKelvy <mmckelvy@gmail.com>
2016-08-28 14:39:32 -0700

    render home page

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,9 +1,13 @@
 import httpHash from 'http-hash';
 import basicStatic from 'basic-static';
 
+import renderView from './views/render-view';
+
 const routes = httpHash();
 const serveStatic = basicStatic({compress: true, cache: 'max-age=31536000'});
 
-routes.set('/', (req, res) => res.end("Hello world!"));
+// Any route should render the index page and let the client take it from there.
+routes.set('*', (req, res) => renderView(req, res, 'index.html'));
+routes.set('/build/client.js', serveStatic);
 
 export default routes;

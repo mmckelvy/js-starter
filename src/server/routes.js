c2dc7b0a1bafd93bdea94482df57f1cb349ce9fb
Mark McKelvy <mmckelvy@gmail.com>
2016-08-31 09:33:32 -0700

    add base styles

--- src/server/routes.js
+++ src/server/routes.js
@@ -4,13 +4,23 @@ import basicStatic from 'basic-static';
 import renderView from './views/render-view';
 
 const routes = httpHash();
-const serveStatic = basicStatic({
+
+const serveBundle = basicStatic({
   cache: 'max-age=31536000',
   compress: true,
 });
 
+const serveAssets = basicStatic({
+  cache: 'max-age=31536000',
+  compress: true,
+  rootDir: __dirname
+});
+
 // Any route should render the index page and let the client take it from there.
 routes.set('*', (req, res) => renderView(req, res, 'index.html'));
-routes.set('/build/client.js', serveStatic);
+
+// Static assets
+routes.set('/build/client.js', serveBundle);
+routes.set('/assets/*', serveAssets);
 
 export default routes;
